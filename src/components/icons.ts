c896bf6a4da08514eab6e71682f3bec58afc597d
Sam Richard <snugug@gmail.com>
2026-04-19 12:45:53 -0400

    feat: add delete icon to icons.ts

--- src/components/icons.ts
+++ src/components/icons.ts
@@ -47,3 +47,15 @@ export function createBarChartSvg(): SVGSVGElement {
   svg.appendChild(path);
   return svg;
 }
+
+export function createDeleteSvg(): SVGSVGElement {
+  const svg = document.createElementNS(ns, 'svg');
+  svg.setAttribute('width', '18');
+  svg.setAttribute('height', '18');
+  svg.setAttribute('viewBox', '0 -960 960 960');
+  svg.setAttribute('fill', 'currentColor');
+  const path = document.createElementNS(ns, 'path');
+  path.setAttribute('d', 'M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z');
+  svg.appendChild(path);
+  return svg;
+}
